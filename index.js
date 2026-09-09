31ac53ff094426db55cbf243ccfb0230acb8a04a
evangelistajulio17-pixel <evangelistajulio17@gmail.com>
2026-09-09 08:57:30 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,55 +1,181 @@
 /*!
- * body-parser
- * Copyright(c) 2014-2015 Douglas Christopher Wilson
+ * range-parser
+ * Copyright(c) 2012-2014 TJ Holowaychuk
+ * Copyright(c) 2015-2016 Douglas Christopher Wilson
  * MIT Licensed
  */
 
 'use strict'
 
 /**
- * @typedef {Object} Parsers
- * @property {Function} json JSON parser
- * @property {Function} raw Raw parser
- * @property {Function} text Text parser
- * @property {Function} urlencoded URL-encoded parser
- */
-
-/**
  * Module exports.
- * @type {Function & Parsers}
- */
-exports = module.exports = bodyParser
-
-/**
- * JSON parser.
  * @public
  */
-exports.json = require('./lib/types/json')
+
+module.exports = rangeParser
 
 /**
- * Raw parser.
- * @public
- */
-exports.raw = require('./lib/types/raw')
-
-/**
- * Text parser.
- * @public
- */
-exports.text = require('./lib/types/text')
-
-/**
- * URL-encoded parser.
- * @public
- */
-exports.urlencoded = require('./lib/types/urlencoded')
-
-/**
- * Create a middleware to parse json and urlencoded bodies.
+ * Parse "Range" header `str` relative to the given file `size`.
  *
- * @deprecated
+ * @param {Number} size
+ * @param {String} str
+ * @param {Object} [options]
+ * @return {Array}
  * @public
  */
-function bodyParser () {
-  throw new Error('The bodyParser() generic has been split into individual middleware to use instead.')
+
+function rangeParser (size, str, options) {
+  if (typeof str !== 'string') {
+    throw new TypeError('argument str must be a string')
+  }
+
+  var index = str.indexOf('=')
+
+  if (index === -1) {
+    return -2
+  }
+
+  // split the range string
+  var arr = str.slice(index + 1).split(',')
+  var ranges = []
+
+  // add ranges type
+  ranges.type = str.slice(0, index)
+
+  // parse all ranges
+  for (var i = 0; i < arr.length; i++) {
+    var indexOf = arr[i].indexOf('-')
+    if (indexOf === -1) {
+      return -2
+    }
+
+    var startStr = arr[i].slice(0, indexOf).trim()
+    var endStr = arr[i].slice(indexOf + 1).trim()
+
+    var start = parsePos(startStr)
+    var end = parsePos(endStr)
+
+    if (startStr.length === 0) {
+      start = size - end
+      end = size - 1
+    } else if (endStr.length === 0) {
+      end = size - 1
+    }
+
+    // limit last-byte-pos to current length
+    if (end > size - 1) {
+      end = size - 1
+    }
+
+    if (isNaN(start) || isNaN(end)) {
+      return -2
+    }
+
+    // invalid or unsatisifiable
+    if (start > end || start < 0) {
+      continue
+    }
+
+    // add range
+    ranges.push({
+      start: start,
+      end: end
+    })
+  }
+
+  if (ranges.length < 1) {
+    // unsatisifiable
+    return -1
+  }
+
+  return options && options.combine
+    ? combineRanges(ranges)
+    : ranges
+}
+
+/**
+ * Parse string to integer.
+ * @private
+ */
+
+function parsePos (str) {
+  if (/^\d+$/.test(str)) return Number(str)
+  return NaN
+}
+
+/**
+ * Combine overlapping & adjacent ranges.
+ * @private
+ */
+
+function combineRanges (ranges) {
+  var ordered = ranges.map(mapWithIndex).sort(sortByRangeStart)
+
+  for (var j = 0, i = 1; i < ordered.length; i++) {
+    var range = ordered[i]
+    var current = ordered[j]
+
+    if (range.start > current.end + 1) {
+      // next range
+      ordered[++j] = range
+    } else if (range.end > current.end) {
+      // extend range
+      current.end = range.end
+      current.index = Math.min(current.index, range.index)
+    }
+  }
+
+  // trim ordered array
+  ordered.length = j + 1
+
+  // generate combined range
+  var combined = ordered.sort(sortByRangeIndex).map(mapWithoutIndex)
+
+  // copy ranges type
+  combined.type = ranges.type
+
+  return combined
+}
+
+/**
+ * Map function to add index value to ranges.
+ * @private
+ */
+
+function mapWithIndex (range, index) {
+  return {
+    start: range.start,
+    end: range.end,
+    index: index
+  }
+}
+
+/**
+ * Map function to remove index value from ranges.
+ * @private
+ */
+
+function mapWithoutIndex (range) {
+  return {
+    start: range.start,
+    end: range.end
+  }
+}
+
+/**
+ * Sort function to sort ranges by index.
+ * @private
+ */
+
+function sortByRangeIndex (a, b) {
+  return a.index - b.index
+}
+
+/**
+ * Sort function to sort ranges by start position.
+ * @private
+ */
+
+function sortByRangeStart (a, b) {
+  return a.start - b.start
 }
